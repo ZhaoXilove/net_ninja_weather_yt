b10d0204f47fc3ab33ef5ad3663d6deb37dd5419
yjf <1787673323@qq.com>
2022-11-21 16:17:48 +0800

    [功能]使用高德map查询天气

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,50 @@
 <template>
-  <div>
-    hello
-  </div>
+  <main class="container text-white">
+    <div class="pt-4 mb-8 relative">
+      <input
+        type="text"
+        v-model="searchQuery"
+        @input="getSearchResults"
+        placeholder="搜素一个城市或省"
+        class="py-2 px-1 w-full bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]"
+      />
+      <ul
+        class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]"
+        v-if="mapGaodeSearchResults.arr.lives"
+        >
+        <li
+          class="py-2 cursor-pointer"
+          :key="searchResult.adcode"
+          v-for="searchResult in mapGaodeSearchResults.arr.lives"
+        >
+          {{ searchResult.province }}-{{ searchResult.city }}
+        </li>
+      </ul>
+    </div>
+  </main>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref, reactive } from "vue";
+import axios from "axios";
+const KEY = ref("0bfebabfb6addc48914d7d6785d5298c");
+const searchQuery = ref("");
+const queryTimeout = ref(null);
+const mapGaodeSearchResults = reactive({ arr: [] });
+const getSearchResults = () => {
+  clearTimeout(queryTimeout.value);
+  queryTimeout.value = setTimeout(async () => {
+    if (searchQuery.value !== "") {
+      const result = await axios.get(
+        `https://restapi.amap.com/v3/weather/weatherInfo?key=${KEY.value}&city=${searchQuery.value}`
+      );
+      mapGaodeSearchResults.arr = result.data;
+      console.log(mapGaodeSearchResults.arr);
+      return;
+    }
+    mapGaodeSearchResults.arr = [];
+  }, 300);
+};
+</script>
 
 <style lang="scss" scoped></style>
